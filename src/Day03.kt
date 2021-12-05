4b52579b7d2326d3adbc00ed5dd2df142ffe3735
Nate Ridderman <nate.ridderman@e-gineering.com>
2021-12-05 14:57:06 -0500

    day 3 part 2 working

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,9 +1,5 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val sumPerPosition: MutableList<Int> = MutableList(input[0].length) {
-            0
-        }
-
         val gammaRateAsString = (0 until input.first().length).joinToString("") { position ->
             input.count { it[position] == '1' }.let { if (it > input.size/2.0f) "1" else "0" }
         }
@@ -22,14 +18,38 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var oxygenGenerator = input.toMutableList()
+        var index = 0
+        while (oxygenGenerator.size > 1) {
+            val numberOfOnes = oxygenGenerator.count { it[index] == '1' }
+            if (numberOfOnes >= oxygenGenerator.size - numberOfOnes) {
+                oxygenGenerator = oxygenGenerator.filter { it[index] == '1' }.toMutableList()
+            } else {
+                oxygenGenerator = oxygenGenerator.filter { it[index] == '0' }.toMutableList()
+            }
+            index++
+        }
+
+        var co2Generator = input.toMutableList()
+        index = 0
+        while (co2Generator.size > 1) {
+            val numberOfOnes = co2Generator.count { it[index] == '1' }
+            if (numberOfOnes < co2Generator.size - numberOfOnes) {
+                co2Generator = co2Generator.filter { it[index] == '1' }.toMutableList()
+            } else {
+                co2Generator = co2Generator.filter { it[index] == '0' }.toMutableList()
+            }
+            index++
+        }
+
+        return Integer.parseInt(oxygenGenerator[0], 2) * Integer.parseInt(co2Generator[0], 2)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
