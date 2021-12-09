b925e15bb9e94d6e2c314154e9672c6df5bc09fc
Nate Ridderman <nate.ridderman@e-gineering.com>
2021-12-09 10:04:15 -0500

    wip on part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -29,20 +29,39 @@ fun main() {
                 }
             }.sum()
         }.sum()
-        //return 0
     }
 
     fun part2(input: List<String>): Int {
+        val rows = input.map {
+            val paddedRow = it.toMutableList()
+            paddedRow.add(0, '9')
+            paddedRow.add('9')
+            paddedRow.toCharArray().map { it.digitToInt() }
+        }.toMutableList()
+        rows.add(0, List(rows[1].size, { 9 }))
+        rows.add(List(rows[1].size, { 9 }))
+
+        val columns = rows.rowToColumn()
+
+        val lowPoints = rows.subList(1, rows.size - 1).mapIndexed { y, row ->
+            row.subList(1, row.size - 1).mapIndexedNotNull { x, i ->
+                val nearby = listOf(rows[y+1][x], rows[y+1][x+2], columns[x+1][y], columns[x+1][y+2])
+                if (i < nearby.minOf { it }) {
+                    Pair(x,y)
+                } else {
+                    null
+                }
+            }
+        }.flatten()
+
         return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
     check(part1(testInput) == 15)
-//    println(part1(testInput))
-
 
     val input = readInput("Day09")
     println(part1(input))
-    println(part2(input))
+    println(part2(testInput))
 }
