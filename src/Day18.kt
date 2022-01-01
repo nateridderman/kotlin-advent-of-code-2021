4751972a5728fc86d9ccbfda997a1bb0c2321f64
Nate Ridderman <nate.ridderman@e-gineering.com>
2022-01-01 15:16:34 -0500

    can't find tricky bug... i think i'm close

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,11 +1,13 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        input.forEach {
+    fun part1(input: List<String>): String {
+        val result = input.reduce { acc, it ->
             println("\nfor $it")
             var reducedSomething = true
-            var newList: String = it
+            var newList = "[$acc,$it]"
             while (reducedSomething) {
                 reducedSomething = false
                 var depth = 0
@@ -21,48 +23,55 @@ fun main() {
                                 val indexOfPrev = newList.substring(0, i).reversed().indexOfFirst { it.isDigit() }
                                 val indexOfNext = newList.substring(i+3).indexOfFirst { it.isDigit() }
 
+                                val debugList = newList
                                 val first = token.digitToInt()
                                 val second = newList[i+2].digitToInt()
 
                                 newList = newList.take(i-1).plus('0').plus(
                                     newList.substring(i+4)
                                 )
+
+                                val frozenList = newList
+
                                 var extraSwap = 0
                                 if (indexOfPrev != -1) {
                                     val newPrev = newList[i - indexOfPrev - 1].digitToInt() + first
-                                    val result = if (newPrev > 10) {
+                                    val result = if (newPrev >= 10) {
                                         extraSwap = 4
-                                        "[" + Math.floor(newPrev/2.0).toInt()  + "," + Math.ceil(newPrev/2.0).toInt() + "]"
+                                        val biggie = BigDecimal(newPrev)
+                                        "[" + biggie.divide(BigDecimal(2), RoundingMode.FLOOR).toInt()  + "," + biggie.divide(BigDecimal(2), RoundingMode.CEILING).toInt() + "]"
                                     } else {
                                         newPrev.toString()
                                     }
                                     newList = newList.take(i - indexOfPrev - 1) + result + newList.substring(i - indexOfPrev )
                                 }
                                 if (indexOfNext != -1) {
-                                    //even this doesn't work because the index is wrong. can i fix by checking the size deltas btw the two?
-                                    val newPrev = newList[i + indexOfNext +extraSwap - 1].digitToInt() + second
-                                    val result = if (newPrev > 10) {
-                                        "[" + Math.floor(newPrev/2.0).toInt()  + "," + Math.ceil(newPrev/2.0).toInt() + "]"
+                                    val newNext = frozenList[i + indexOfNext - 1].digitToInt() + second
+                                    val result = if (newNext >= 10) {
+                                        val biggie = BigDecimal(newNext)
+                                        "[" + biggie.divide(BigDecimal(2), RoundingMode.FLOOR).toInt()  + "," + biggie.divide(BigDecimal(2), RoundingMode.CEILING).toInt() + "]"
                                     } else {
-                                        newPrev.toString()
+                                        newNext.toString()
                                     }
                                     newList = newList.take(i + indexOfNext + extraSwap - 1) + result + newList.substring(i + indexOfNext + extraSwap)
-                                    //newList = StringBuilder(newList).also { it.setCharAt(i + indexOfNext - 1, newList[i + indexOfNext -1].plus(second)) }.toString()
                                 }
                                 reducedSomething = true
+                                println(newList)
                             }
                         } else {
+                            check(token == ',')
                             //it's a comma
                         }
                     }
                 }
             }
             println("newList after reduction: $newList")
-
+            newList
         }
 
+        println(result)
         //If any regular number is 10 or greater, the leftmost such regular number splits.
-        return 0
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -70,8 +79,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day18_test")
-    check(part1(testInput) == 0)
+    val testInput = readInput("Day18_test3")
+    check(part1(testInput) == "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]")
 
     val input = readInput("Day18")
 //    println(part1(input))
@@ -81,3 +90,10 @@ fun main() {
 //[[3,[2,[8,0]]],[9,[5,[7,0]]]]
 
 //[[[[0,7],4],[[7,8],[6,0]]],[8,1]]
+
+//[[[[4,0],[5,4]],[[7,7],[6,0]]],[[[6,6],[0,5]],[[5,[6,6]],[0,[7,[7,8]]]]]]
+
+
+//[[[[4,0],[5,4]],[[7,7],[6,0]]],[[[6,6],[5,5]],[[0,6],[[6,7],0]]]]
+//turned into
+//[[[[4,0],[5,4]],[[7,7],[6,0]]],[[[6,6],[5,5]],[[0,[6,6]],[0,7]]]]
